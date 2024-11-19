11fc80f1b8791cb6ccafe384557956361edbf538
nakashima1231 <gabrielmnakashima2@gmail.com>
2024-11-19 12:56:40 -0300

    projeto alura flashcard concluido

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+CriaCartao(
+    "Programação",
+    "O que é Python?",
+    "O JavaScript é uma linguagem de programação"
+)
+
+CriaCartao(
+    "Geografia",
+    "Qual a capital da França?",
+    "A capital da França é Paris"
+)
+
+CriaCartao(
+    "Programação",
+    "O que é uma função?",
+    "Uma função é um bloco de código que executa uma tarefa"
+)
+
+CriaCartao(
+    "Língua inglesa",
+    "Como se diz oi em Inglês?",
+    "Oi em Inglês é hi (RAI)"
+)
